bb4ec511c3938fa1e1e714fbaa047760a0254bf0
lexiewh <lexwhite028@gmail.com>
2021-11-11 13:39:00 -0600

    more quote styling

--- src/components/QuoteBox.js
+++ src/components/QuoteBox.js
@@ -1,13 +1,19 @@
-import Button from 'react-bootstrap/Button'
+import { Button, Container, Row, Col} from 'react-bootstrap'
 
 function QuoteBox() {
     return (
-        <div id="quote-box">
-            <h3 id="text">Quote</h3>
-            <p id="author">Author</p>
-            <Button id="new-quote">New Quote</Button>
-            <a href="twitter.com/intent/tweet">Tweet</a>
-        </div>
+        <Container id="quote-box">
+            <Row>
+                <h3 id="text">Quote</h3>
+            </Row>
+            <Row>
+                <p id="author">Author</p>
+            </Row>
+            <Row>
+                <Col><a class="twitter-share-button" href="https://twitter.com/intent/tweet">Tweet</a></Col>
+                <Col id="btn-col"><Button id="new-quote">New quote</Button></Col>
+            </Row>
+        </Container>
     )
 }
 
